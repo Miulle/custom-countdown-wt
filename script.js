e8a4c9aa8f4bd7b7e54c56e9d574bce1d0556d88
Miulle <mika.strandb@gmail.com>
2020-12-09 20:22:16 +0100

    Populate Countdown View

--- script.js
+++ script.js
@@ -2,12 +2,46 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdownForm');
 const dateEl = document.getElementById('date-picker');
 
+const countdownEl = document.getElementById('countdown');
+const countdownElTitle = document.getElementById('countdown-title');
+const countdownBtn = document.getElementById('countdown-button');
+const timeElements = document.querySelectorAll('span');
+
 let countdownTitle = '';
 let countdownDate = '';
+let countdownValue = Date;
 
-// Set Date Input Min with Today'S Date
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+
+// Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Populate Countdown / Complete UI
+function updateDOM() {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+    
+    // Populate Countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
+
+    // Hide Input
+    inputContainer.hidden = true;
+    // Show Countdown
+    countdownEl.hidden = false;
+}
 
 // Take Values From Form Input
 function updateCountdown(e) {
@@ -15,6 +49,9 @@ function updateCountdown(e) {
     countdownTitle = this[0].value;
     countdownDate = this[1].value;
     console.log(countdownTitle, countdownDate);
+    // Get number version of current Date, updateDOM
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
 }
 
 // Event Listener
